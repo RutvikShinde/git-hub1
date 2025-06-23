4bc2abfc02d5a80fed5e3546180aaba64e848a29
unknown <rutvikshinde09@gmail.com>
2025-06-23 13:20:58 +0530

    loop

---
+++ loop1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int n,i;
+    printf("enter a number");
+    scanf("%d",&n);
+    i=0;
+    while(i<=n)
+    {
+        printf("hello world\n");
+        i++;
+    }
+    return 0;
+
+}
