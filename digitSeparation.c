175fd95c061441eb43ba04e422b5f87b8f520ec3
unknown <rutvikshinde09@gmail.com>
2025-06-25 15:08:20 +0530

    digit sepration

---
+++ digitSeparation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int n,rem=0;
+    printf("enter a number");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        rem =n%10;
+        printf("%d  ",rem);
+        n/=10;
+    }
+    return 0;
+
+}
